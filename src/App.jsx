2f4003cfa83ea510834c69b2d124ab354dbf84fa
FreemindNation <freemindnation@outlook.com>
2024-08-26 13:52:14 +0100

    Install mui

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,11 @@ import Layout from './components/Layout';
 import ErrorPage from './components/ErrorPage';
 import { ErrorProvider } from './contexts/ErrorContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 
 const App = () => {
