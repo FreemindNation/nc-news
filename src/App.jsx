a12749ab108c038be1b4e548a9507572535fbad7
FreemindNation <freemindnation@outlook.com>
2024-08-27 22:32:21 +0100

    Add grid with material ui

--- src/App.jsx
+++ src/App.jsx
@@ -14,29 +14,31 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
+import  Grid2  from '@mui/material/Unstable_Grid2/Grid2';
 
 
 const App = () => {
    
 
   return (
-    <UserProvider>
-      <ErrorProvider>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path='/' element={<Login />} />
-            <Route path='*' element={<ErrorPage />}/>
-            <Route element={<Layout/>}>
-              <Route path='/articles/' element={<ArticlesList />} />
-              <Route path='/articles/:article_id' element={<FullArticle />} />
-              <Route path='/topics/:slug' element={<ArticlesList />}/>
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </ErrorProvider>
-    </UserProvider>
+    <Grid2 container spacing={2}>
+      <UserProvider>
+        <ErrorProvider>
+          <BrowserRouter>
+            <Header />
+            <Routes>
+              <Route path='/' element={<Login />} />
+              <Route path='*' element={<ErrorPage />}/>
+              <Route element={<Layout/>}>
+                <Route path='/articles/' element={<ArticlesList />} />
+                <Route path='/articles/:article_id' element={<FullArticle />} />
+                <Route path='/topics/:slug' element={<ArticlesList />}/>
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </ErrorProvider>
+      </UserProvider>
+    </Grid2>
   )
 }
 
