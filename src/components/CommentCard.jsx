26063ba3bcb5a62bd5f7a8e96a59bde00ae10cf2
FreemindNation <freemindnation@outlook.com>
2024-08-28 23:42:11 +0100

    Edit CommentCard and style it with material ui

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -3,6 +3,8 @@ import { timeConverter } from "../utils/time-converter";
 import { UserContext } from "../contexts/UserContext";
 import { useContext, useState } from "react";
 import { deleteComment } from "../utils/api-calls";
+import { Divider, Avatar, Grid, Paper  } from "@mui/material";
+
 const CommentCard = ({ comment, comments, setComments, article, setArticle, index })=> {
 
     const { user, setUser } = useContext(UserContext);
@@ -28,18 +30,18 @@ const CommentCard = ({ comment, comments, setComments, article, setArticle, inde
         })
     }
     return (
-        <Card style={{width: '18em'}}>
-            <Card.Body>
-                <Card.Text>
-                    {comment.body}
-                </Card.Text>
-            </Card.Body>
-            <Card.Footer className="card-footer">
-                <small className="text=muted">By {comment.author} on {timeConverter(comment.created_at)}</small>
-                <small>Votes: {comment.votes}</small>
-            </Card.Footer>
+        <Paper style={{ padding: "40px 20px", backgroundColor: 'tomato' }}>
+            <Grid container wrap="nowrap" spacing={2}>
+                <Grid justifyContent={"left"} item xs zeroMinWidth>
+                    <h5 style={{ margin: 0, textAlign: "left" }}>{comment.author}</h5>
+                    <p style={{ textAlign: "left" }}>{comment.body}</p>
+                    <p style={{ textAlign: "left", color: "gray" }}>{timeConverter(comment.created_at)}</p>
+                    <p style={{ textAlign: "left", color: "gray" }}>Votes: {comment.votes}</p>
+                </Grid>
+            </Grid>
             {user === comment.author ? <button onClick={handleDelete} disabled={isDeletingComment}>{isDeletingComment ? 'Deleting comment...' : 'Delete comment'}</button> : null}
-        </Card>
+            <Divider variant="fullWidth" style={{ margin: "30px 0", border: '1px solid black' }} />
+        </Paper>
 
     )
 }
